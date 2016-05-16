9aec6275c61f4eb60ff6c7e010ade00094db4ab3
jbales <jrbdeveloper@gmail.com>
2016-05-15 22:21:28 -0700

    adding a verticle for the client to property matching

---
+++ ClientManager/ClientManager/Models/MatchModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ClientManager.Models
+{
+    public class MatchModel
+    {
+        public ClientModel Client { get; set; }
+
+        public PropertyModel Property { get; set; }
+    }
+}
